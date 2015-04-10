24af897eab9bad86cc880c783adfef99199ab0ff
JS Lover <poshtehani+startjs@gmail.com>
2015-04-10 23:17:35 +0430

    11 : checkinput

---
+++ 11.checkinput.js
@@ -0,0 +1,18 @@
+function whatstype(para){
+	console.log(typeof(para))
+}
+
+var obj = {
+	a: "A",
+	b: "B"
+}
+whatstype(obj)
+
+var str = "No body cares!"
+whatstype(str)
+
+var bool = false
+whatstype(bool)
+
+var num = 2048
+whatstype(num)
