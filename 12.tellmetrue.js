2f1a35d5b36626342d7d9eaa2d0baa41f7151a18
JS Lover <poshtehani+startjs@gmail.com>
2015-04-12 15:03:18 +0430

    hoisting

---
+++ 12.tellmetrue.js
@@ -0,0 +1,20 @@
+var x = 100
+
+tellme()
+tellmemore()
+// xy()
+
+function tellme(){
+
+	console.log(x)
+}
+
+function tellmemore(){
+	x = 200
+	var x;
+	console.log(x)
+}
+
+/*var xy = function(){
+	console.log('XY');
+}*/
