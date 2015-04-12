9d1b03ed4faacef551ce962cde258b35c7240522
JS Lover <poshtehani+startjs@gmail.com>
2015-04-12 16:41:50 +0430

    switch

---
+++ 14.operate.js
@@ -0,0 +1,15 @@
+var name
+
+name = prompt("Whats your name?")
+
+switch(name){
+	case "jim" :
+		console.log("Hello jim");
+		break;
+	case "jack" :
+		console.log("Hello jack");
+		break;
+	default :
+		console.log("I dont know you.")
+		break;
+}
